3fd0ad8c332e890fa3a91d7b6fa726f8ca00c2c8
Ibrahima Bah <bibrahim97@outlook.com>
2023-02-20 23:38:42 +0300

    react track

--- React/react.js
+++ React/react.js
@@ -1,5 +1,5 @@
 const JSX = <h1>Hello JSX!</h1>;
-const JSXelements = 
+const JSXelements = (
 <div>
   <h1>JSX</h1>
   <p>Welcome To React</p>
@@ -9,4 +9,20 @@ const JSXelements =
     <li>React Components</li>
     <li>Valid JSX methods</li>
   </ul>
-</div>;
+</div>,
+
+<div className="myDiv">
+      <h1>Hello World</h1>
+      <p>Lets render this to the DOM</p>
+</div>,
+
+
+<div>
+    <h2>Welcome to React!</h2> <br />
+    <p>Be sure to close all tags!</p>
+    <hr />
+</div>
+);
+
+// Render HTML Elements to the DOM
+ReactDOM.render(JSX, document.getElementById('challenge-node'));
