2384275e2ba8ee9a2b4d3eefc8b67914135b58a6
Ibrahima Bah <bibrahim97@outlook.com>
2023-02-20 19:58:31 +0300

    react track

--- React/react.js
+++ React/react.js
@@ -1,1 +1,11 @@
 const JSX = <h1>Hello JSX!</h1>;
+const JSXelements = 
+<div>
+  <h1>JSX</h1>
+  <p>Welcome To React</p>
+  <ul>
+    <li>JSX Elements</li>
+    <li>React Components</li>
+    <li>Valid JSX methods</li>
+  </ul>
+</div>;
