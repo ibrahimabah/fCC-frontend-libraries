a1de8f4f1553a09916237e5a89353f9656891f23
Ibrahima Bah <bibrahim97@outlook.com>
2023-04-02 12:19:57 +0300

    react track

--- React/react.js
+++ React/react.js
@@ -250,7 +250,7 @@ class GateKeeper extends React.Component {
     };
     // Change code below this line
     if(this.state.input.length > 15){
-      
+
       inputStyle.border = '3px solid red'
     }
     // Change code above this line
@@ -266,3 +266,51 @@ class GateKeeper extends React.Component {
     );
   }
 };
+
+// Use Array.map() to Dynamically Render Elements
+const textAreaStyles = {
+  width: 235,
+  margin: 5
+};
+
+class MyToDoList extends React.Component {
+  constructor(props) {
+    super(props);
+    // Change code below this line
+    this.state = {
+      userInput: '',
+      toDoList: []
+    }
+    // Change code above this line
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleSubmit() {
+    const itemsArray = this.state.userInput.split(',');
+    this.setState({
+      toDoList: itemsArray
+    });
+  }
+  handleChange(e) {
+    this.setState({
+      userInput: e.target.value
+    });
+  }
+  render() {
+    const items = this.state.toDoList.map(i => <li>{i}</li>); // Change this line
+    return (
+      <div>
+        <textarea
+          onChange={this.handleChange}
+          value={this.state.userInput}
+          style={textAreaStyles}
+          placeholder='Separate Items With Commas'
+        />
+        <br />
+        <button onClick={this.handleSubmit}>Create List</button>
+        <h1>My "To Do" List:</h1>
+        <ul>{items}</ul>
+      </div>
+    );
+  }
+}
