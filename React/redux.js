2596fc671ad204c81fefb08273b67b57adab66cc
Ibrahima Bah <bibrahim97@outlook.com>
2023-04-03 20:36:14 +0300

    redux: get state from the redux store

--- React/redux.js
+++ React/redux.js
@@ -4,12 +4,19 @@ const reducer = (state = 5) => {
 }
 /*
     There are three parts to this challenge:
-    1: Declaring the "store" variable
-    2: Assigning it to the "createStore()" method.
-    3: And Passing in the "reducer" as an argument.
+    (1) Declaring the "store" variable
+    (2) Assigning it to the "createStore()" method
+    (3) And Passing in the "reducer" as an argument
 */
 const store = Redux.createStore(reducer);
 
 //  >   >   >   >   >   Get State from the Redux Store  <   <   <   <   <
+store = Redux.createStore(
+    (state = 5) => state
+  );
+/*
+    Retrieving data from "store" by using the "getState()"" method.
+ */
+let currentState = store.getState();
 
 
