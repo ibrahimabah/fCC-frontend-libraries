7cb9ec333ebafb3d12c324b22755a2f2a0a2103f
Ibrahima Bah <bibrahim97@outlook.com>
2023-04-08 03:19:38 +0300

    - (Redux) - Remove an Item from an Array

--- React/redux.js
+++ React/redux.js
@@ -461,4 +461,25 @@ immutableReducer = (state = ['Do not mutate state!'], action) => {
   store = Redux.createStore(immutableReducer);
   //  ⬇   ⬇   ⬇   ⬇   ⬇
 
+//  >   >   >   >   >   Remove an Item from an Array    <   <   <   <   <
+immutableReducer = (state = [0,1,2,3,4,5], action) => {
+    switch(action.type) {
+      case 'REMOVE_ITEM':
+        // Don't mutate state here or the tests will fail
+        return [
+            ...state.slice(0, action.index).concat(state.slice(action.index + 1, state.length))
+          ]; // Finish writing the reducer so a new state array is returned with the item at the specific index removed
+      default:
+        return state;
+    }
+  };
   
+  const removeItem = (index) => {
+    return {
+      type: 'REMOVE_ITEM',
+      index
+    }
+  }
+  
+  store = Redux.createStore(immutableReducer);
+  //  ⬇   ⬇   ⬇   ⬇   ⬇
