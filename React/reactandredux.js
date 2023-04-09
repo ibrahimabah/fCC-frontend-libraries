846f2dd5ebf48f0b73177720354f3eacc1224a53
Ibrahima Bah <bibrahim97@outlook.com>
2023-04-09 08:24:28 +0300

    -(React and Redux)- Manage State Locally First

--- React/reactandredux.js
+++ React/reactandredux.js
@@ -10,11 +10,43 @@ class DisplayMessages extends React.Component {
         messages: []
       }
     }// end of "Getting Started with React Redux" challenge
-    render() {
-      return <div />
-    }
-  };
-  //  ⬇   ⬇   ⬇   ⬇   ⬇
+    //  ⬇   ⬇   ⬇   ⬇   ⬇
 
-//  >   >   >   >   >   Manage State Locally First  <   <   <   <   <
+    //  >   >   >   >   >   Manage State Locally First  <   <   <   <   <
+    // Add handleChange() and submitMessage() methods here
+    handleChange(event){
+        this.setState({
+          input: event.target.value,
+          messages: this.state.messages
+        })// The handleChange() method should update the input with what the user is typing
+      }
+      
+      submitMessage(){
+        this.setState({
+          input: '',
+          messages: [...this.state.messages, this.state.input]
+        })// The submitMessage() method should concatenate the current message (stored in input) to the messages array in local state, and clear the value of the input.
+      }
+
+    render() {
+    return (
+      <div>
+        <h2>Type in a new Message:</h2>
+        { /* Render an input, button, and ul below this line */ }
+        <input onChange={this.handleChange.bind(this)} value={this.state.input}/>{/* When the input element changes, it should trigger a handleChange() method and should render the value of input that's in the component's state. */}
+        <button onClick={this.submitMessage.bind(this)}>Submit</button>{/* button element should trigger a submitMessage() method when it's clicked */}
+        <ul>
+          {this.state.messages.map((x, i) =>{//use the ul to map over the array of messages and render it to the screen as a list of li elements.
+            return <li key={i}>{x}</li>
+          })}
+        </ul>
+        { /* Change code above this line */ }
+      </div> // end of "Manage State Locally First" challenge
+    //  ⬇   ⬇   ⬇   ⬇   ⬇  
+    );
+  }
+  };
+  
+
+
 
