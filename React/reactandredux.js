830d983918294ac809a40508d686c8746acafd31
Ibrahima Bah <bibrahim97@outlook.com>
2023-04-09 07:40:21 +0300

    -(React and Redux)- Getting Started with React Redux

--- React/reactandredux.js
+++ React/reactandredux.js
@@ -1,1 +1,18 @@
 //   => =>  =>  =>  =>  REACT and REDUX   <=  <=  <=  <=  <=
+
+//  >   >   >   >   >   Getting Started with React Redux    <   <   <   <   <
+class DisplayMessages extends React.Component {
+    // Change code below this line
+    constructor(props){ // Add a constructor to this component
+      super(props); // constructor should be called with super, passing in props
+      this.state={ // initialize it with a state that has an input set to an empty string, and messages that's set to an empty array
+        input: "",
+        messages: []
+      }
+    }
+    // Change code above this line
+    render() {
+      return <div />
+    }
+  };
+  //  ⬇   ⬇   ⬇   ⬇   ⬇
