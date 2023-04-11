73752fb4eb0919d60781c4e3d9c58c5027ffd958
Ibrahima Bah <bibrahim97@outlook.com>
2023-04-11 12:13:32 +0300

    - (React & Redux) - Moving Forward From Here

--- React/reactandredux.js
+++ React/reactandredux.js
@@ -138,3 +138,36 @@ class AppWrapper extends React.Component {
 //  >   >   >   >   >   Extract Local State into Redux  <   <   <   <   <
 // updates on freeCodeCamp
 //  ⬇   ⬇   ⬇   ⬇   ⬇
+
+//  >   >   >   >   >   Moving Forward From here  <   <   <   <   <
+/*
+    Helpful links:
+    * https://www.freecodecamp.org/news/install-react-with-create-react-app/
+ */
+/*
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider, connect } from 'react-redux'
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+
+import rootReducer from './redux/reducers'
+import App from './components/App'
+
+const store = createStore(
+  rootReducer,
+  applyMiddleware(thunk)
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App/>
+  </Provider>,
+  document.getElementById('root')
+);
+*/
+
+// Only change code below this line
+console.log('Now I know React and Redux!')
+//  ⬇   ⬇   ⬇   ⬇   ⬇
+
